309fc676fbee5e34a85a9fac5b4986c7502d1c46
Viona Djunaidy <vionadjunaidy@gmail.com>
2025-05-30 20:30:34 +0700

    deploy cicd pipeline
    Signed-off-by: Viona Djunaidy <vionadjunaidy@gmail.com>

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,10 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    server: {
+      proxy: {
+        "/service": {
+          target: mode === "development"
+            ? "http://localhost:5000"
+            : "https://e2425-wads-l4bcg2-server-viona.csbihub.id",
+          changeOrigin: true,
+          secure: mode !== "development",
+          ws: true,
+        },
+      },
+    },
+    build: {
+      outDir: "dist",
+    },
+    plugins: [react(), tailwindcss()],
+  }
 })
